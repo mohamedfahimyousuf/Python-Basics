827adfa090dfd36d7202d990184c564d053fce06
mohamedfahimyousuf <mohamedfahimyousuf786@gmail.com>
2024-09-04 22:31:58 +0530

    code uploaded

--- LastDigitNumber.py
+++ LastDigitNumber.py
@@ -1,1 +1,13 @@
+def firstDigit(num):
+    while num>=10:
+        num=num/10
+    print("First number: ",int(num))
+
+def lastDigit(num):
+    print("Last Number: ",num%10)
+
+num =int(input("Enter a number: "))    
+
+firstDigit(num)
+lastDigit(num)
 
