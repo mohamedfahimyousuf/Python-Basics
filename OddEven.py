97a2d67bc4e430ebc66fbfc77876e81497fa048a
mohamedfahimyousuf <mohamedfahimyousuf786@gmail.com>
2024-09-04 22:03:08 +0530

    function added

--- OddEven.py
+++ OddEven.py
@@ -1,5 +1,8 @@
-num= int(input("enter a number: "))
-if num%2==0:
-    print("even")
-else:
-    print("odd")
+def OddEven():
+    num= int(input("enter a number: "))
+    if num%2==0:
+      print("The given number is even")
+    else:
+      print("The given number is odd")
+
+OddEven()
